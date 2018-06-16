a07520aa0037b48e4ed92c24a8b21d8d34958e41
wangbin2017code <32503074+wangbin2017code@users.noreply.github.com>
2018-06-16 10:08:46 +0800

    update

--- src/main/java/com/wangbin/controller/HelloController.java
+++ src/main/java/com/wangbin/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.wangbin.controller;
 import com.wangbin.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author wangbin
  * @date 2018/6/12 23:27
  */
-@RestController
+@Controller
 public class HelloController {
 
     @Value("${sport}")
@@ -28,6 +29,6 @@ public class HelloController {
 
     @RequestMapping(value = "hello",method = RequestMethod.GET)
     public String sayHello(){
-        return student.toString();
+        return "index";
     }
 }
